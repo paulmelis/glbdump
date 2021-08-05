ecaf8d901d399f10e79784075db25fdc52f9b190
Paul Melis <p.e.c.melis@alumnus.utwente.nl>
2021-08-05 17:37:29 +0200

    Guard against missing JSON element Tested with some of the glTF 2.0 sample models from Khronos

--- glbstats.py
+++ glbstats.py
@@ -40,6 +40,7 @@ buffers = j['buffers']
 bufferviews = j['bufferViews']
 accessors = j['accessors']
 
+print()
 print('Asset version %s' % j['asset']['version'])
 print('Asset generator "%s"' % j['asset']['generator'])
 if j['asset']['version'] != '2.0':
@@ -48,14 +49,19 @@ if j['asset']['version'] != '2.0':
 print()
 print('Elements:')
 print('%4d nodes' % len(j['nodes']))
-print('%4d cameras' % len(j['cameras']))
-totalimsize = 0
-for i in j['images']:
-    totalimsize += bufferviews[i['bufferView']]['byteLength']
-print('%4d images     (total %s bytes)' % (len(j['images']), format(totalimsize, ',d')))
-print('%4d materials' % len(j['materials']))
-print('%4d meshes' % len(j['meshes']))
-print('%4d textures' % len(j['textures']))
+if 'cameras' in j:
+    print('%4d cameras' % len(j['cameras']))
+if 'images' in j:
+    totalimsize = 0
+    for i in j['images']:
+        totalimsize += bufferviews[i['bufferView']]['byteLength']
+    print('%4d images     (total %s bytes)' % (len(j['images']), format(totalimsize, ',d')))
+if 'materials' in j:
+    print('%4d materials' % len(j['materials']))
+if 'meshes' in j:    
+    print('%4d meshes' % len(j['meshes']))
+if 'textures' in j:
+    print('%4d textures' % len(j['textures']))
 print()
 
 totalbufsize = 0
@@ -68,63 +74,68 @@ for a in accessors:
     totalaccsize += bufferviews[a['bufferView']]['byteLength']
 print('%4d accessors  (total %s bytes)' % (len(accessors), format(totalaccsize, ',d')))
 
-print()
-print('Images:')
-for idx, i in enumerate(j['images']):
-    bv = i['bufferView']
-    length = bufferviews[bv]['byteLength']
-    print('[%4d] %11s bytes  %-12s "%s"' % (idx, format(length,',d'), i['mimeType'], i['name']))
+if 'images' in j:
+    print()
+    print('Images:')
+    for idx, i in enumerate(j['images']):
+        bv = i['bufferView']
+        length = bufferviews[bv]['byteLength']
+        name = i['name'] if '"name"' in i else '<unnamed>'
+        print('[%4d] %11s bytes  %-12s %s' % (idx, format(length,',d'), i['mimeType'], name))
     
-print()
-print('Meshes:')
-for idx, m in enumerate(j['meshes']):
-    vertices = 0
-    normals = 0
-    color0 = 0
-    texcoord0 = 0
-    texcoord1 = 0
-    indices = 0
-    modes = set([])
-    for p in m['primitives']:
-        attrs = p['attributes']
-        vertices += accessors[attrs['POSITION']]['count']
-        if 'NORMAL' in attrs:
-            normals += accessors[attrs['NORMAL']]['count']
-        if 'COLOR_0' in attrs:
-            color0 += accessors[attrs['COLOR_0']]['count']
-        if 'TEXCOORD_0' in attrs:
-            texcoord0 += accessors[attrs['TEXCOORD_0']]['count']
-        if 'TEXCOORD_1' in attrs:
-            texcoord1 += accessors[attrs['TEXCOORD_1']]['count']
-        indices += accessors[p['indices']]['count']
-        if 'mode' in p:
-            modes.add(p['mode'])
-        else:
-            # Triangles by default
-            modes.add(4)
+if 'meshes' in j:
+    print()
+    print('Meshes:')
+    for idx, m in enumerate(j['meshes']):
+        vertices = 0
+        normals = 0
+        color0 = 0
+        texcoord0 = 0
+        texcoord1 = 0
+        indices = 0
+        modes = set([])
+        for p in m['primitives']:
+            attrs = p['attributes']
+            vertices += accessors[attrs['POSITION']]['count']
+            if 'NORMAL' in attrs:
+                normals += accessors[attrs['NORMAL']]['count']
+            if 'COLOR_0' in attrs:
+                color0 += accessors[attrs['COLOR_0']]['count']
+            if 'TEXCOORD_0' in attrs:
+                texcoord0 += accessors[attrs['TEXCOORD_0']]['count']
+            if 'TEXCOORD_1' in attrs:
+                texcoord1 += accessors[attrs['TEXCOORD_1']]['count']
+            indices += accessors[p['indices']]['count']
+            if 'mode' in p:
+                modes.add(p['mode'])
+            else:
+                # Triangles by default
+                modes.add(4)
+            
+        # See primitive.mode in gltf spec
+        modechars = ['P', 'L', 'LL', 'LS', 'T', 'TS', 'TF']
+        modes = ','.join(map(lambda m: modechars[m], modes))
         
-    # See primitive.mode in gltf spec
-    modechars = ['P', 'L', 'LL', 'LS', 'T', 'TS', 'TF']
-    modes = ','.join(map(lambda m: modechars[m], modes))
-    
-    s = '[%4d] %-20s  %4dP %-5s %8sV %8sI' % \
-        (idx, '"'+m['name']+'"', len(m['primitives']), modes, format(vertices, ',d'), format(indices, ',d'))
-    
-    if color0 > 0:
-        s += ' %8sC0' % format(color0, ',d')
-    if normals > 0:
-        s += ' %8sN' % format(normals, ',d')
-    if texcoord0 > 0:
-        s += ' %8sT0' % format(texcoord0, ',d')
-    if texcoord1 > 0:
-        s += ' %8sT1' % format(texcoord1, ',d')
+        s = '[%4d] %-25s  %4dP %-5s %8sV %8sI' % \
+            (idx, '"'+m['name']+'"', len(m['primitives']), modes, format(vertices, ',d'), format(indices, ',d'))
         
-    print(s)
+        if color0 > 0:
+            s += ' %8sC0' % format(color0, ',d')
+        if normals > 0:
+            s += ' %8sN' % format(normals, ',d')
+        if texcoord0 > 0:
+            s += ' %8sT0' % format(texcoord0, ',d')
+        if texcoord1 > 0:
+            s += ' %8sT1' % format(texcoord1, ',d')
+            
+        print(s)
 
-print()
-print('Materials:')
-for idx, m in enumerate(j['materials']):
-    print('[%4d] "%s"' % (idx, m['name']))
+if 'materials' in j:
+    print()
+    print('Materials:')
+    for idx, m in enumerate(j['materials']):
+        name = '"'+m['name']+'"' if 'name' in m else '<unnamed>'
+        print('[%4d] %s' % (idx, name))
 
 print()
 print('Buffers:')
